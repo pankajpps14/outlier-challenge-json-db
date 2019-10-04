9d541f1aa5bd1eaaba439faa11b5468f49983222
Pankaj Singh <singh.pankajpratap@gmail.com>
2019-10-04 23:57:47 +0530

    intial commit for failed test cases

--- test.js
+++ test.js
@@ -15,6 +15,67 @@ tape('health', async function (t) {
   })
 })
 
+// PUT endpoint test-cases
+
+tape('PUT /:student-id/:propertyName creates a studentId.json file with id as name', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('PUT /:student-id/:propertyName sets property', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('PUT /:student-id/:propertyName(/:propertyName) set nested properties ', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+// GET endpoint test-cases
+
+tape('GET /:student-id/:propertyName retrieves data from studentId.json', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('GET /:student-id/:propertyName(/:propertyName) retrieves nested property', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('GET /:student-id/:propertyName returns 404 if the property does not exist', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('GET /:student-id/:propertyName returns 404 if the file not exist', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+// DELETE endpoint test-cases
+
+tape('DELETE /:student-id/:propertyName removes data from studentId.json', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('DELETE /:student-id/:propertyName(/:propertyName) removes nested property from json', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('DELETE /:student-id/:propertyName returns 404 if the property does not exist', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
+tape('DELETE /:student-id/:propertyName returns 404 if the file not exist', async function (t) {
+  t.fail('not implemented')
+  t.end()
+})
+
 tape('cleanup', function (t) {
   server.close()
   t.end()
